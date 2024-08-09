af8f85767035b4cc345c3f9093206b6908e084ed
sgkochar <sgkochar@gmail.com>
2024-08-09 14:27:06 +0300

    add About, Services and Testimonials blocks to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,6 +38,176 @@ const Home = () => {
           <a href="" className="features-item-link">Read more</a>
         </div>
       </div>
+      <section className='home-about d-f jc-sb align-center'>
+        <div>
+          <div className="above">About Us</div>
+          <h2 className="bl-title home-about-title">The Beauty is about being Comfortable in your own skin!</h2>
+          <p className="home-about-desc">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+            alteration in some form, buying to injected humour, or randomised words which don't look even
+            many desktop publishing packages.
+          </p>
+          <div className="home-about-points d-f">
+            <div className="about-points-item">
+              <div><img src="/images/about-points1.png" alt="about-points1" /></div>
+              <div className="points-item-title">Beauty Experts</div>
+            </div>
+            <div className="about-points-item">
+              <div><img src="/images/about-points2.png" alt="about-points2" /></div>
+              <div className="points-item-title">Great Services</div>
+            </div>
+            <div className="about-points-item">
+              <div><img src="/images/about-points3.png" alt="about-points3" /></div>
+              <div className="points-item-title">100% Genuine</div>
+            </div>
+          </div>
+        </div>
+        <div><img src="/images/home-about.png" alt="home-about" /></div>
+      </section>
+      <section className='home-services'>
+        <div className="above home-services-above">Our services</div>
+        <h2 className="bl-title home-services-title">We are Experienced in making you very Beautiful</h2>
+        <div className="d-f jc-sb">
+          <div><img src="/images/home-services1.png" alt="home-services1" /></div>
+          <div style={{ marginTop: "24px" }}>
+            <div className="service-content d-f">
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Face masks </span>
+                  <span className='service-divider'>....................................... </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$48.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Full body massage </span>
+                  <span className='service-divider'>............................. </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$54.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Geothermal spa </span>
+                  <span className='service-divider'>................................. </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$54.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Sauna relax </span>
+                  <span className='service-divider'>....................................... </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$73.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+            </div>
+            <a href="" className="home-services-link">Get an appointment</a>
+          </div>
+        </div>
+        <div className="d-f jc-sb align-center" style={{ marginTop: "-84px", marginLeft: "39px", marginRight: "-81px" }}>
+          <div>
+            <div className="service-content d-f">
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Face masks </span>
+                  <span className='service-divider'>....................................... </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$48.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Full body massage </span>
+                  <span className='service-divider'>............................. </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$54.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Geothermal spa </span>
+                  <span className='service-divider'>................................. </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$54.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+              <div className='service-content-bl'>
+                <div className='service-info'>
+                  <span className='service-title'>Sauna relax </span>
+                  <span className='service-divider'>....................................... </span>
+                  <span className='service-from'>from </span>
+                  <span className='service-price'>$73.00</span>
+                </div>
+                <p className="service-note">The versions have evolved over the sometimes by accident.</p>
+              </div>
+            </div>
+            <a href="" className="home-services-link">Get an appointment</a>
+          </div>
+          <div><img src="/images/home-services2.png" alt="home-services2" /></div>
+        </div>
+      </section>
+      <section className='home-testimonials'>
+        <div className="above home-testimonials-above">Testimonials</div>
+        <h2 className="bl-title home-testimonials-title">What our Customers says...</h2>
+        <div className="home-testimonials-box d-f jc-sb">
+          <div className="home-testimonials-item">
+            <p className="testimonials-item-text">
+              “ It is a long established fact that a reader will be tracked distracted
+              by the readable content of a page is when looking at its layout. The point of using Lorem of distribution it look like readable English “
+            </p>
+            <div className="testimonials-item-user d-f jc-sb">
+              <div className='d-f'>
+                <div><img src="/images/testimonial-author1.png" alt="testimonial-author1" /></div>
+                <div style={{marginLeft: "20px"}}>
+                  <div className="testimonials-item-rating d-f">
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star-half.png" alt="star-half" /></div>
+                  </div>
+                  <div className="user-name">James Williams</div>
+                  <div className="user-country">United States</div>
+                </div>
+              </div>
+              <div><img src="/images/quote.png" alt="quote" /></div>
+            </div>
+          </div>
+          <div className="home-testimonials-item">
+            <p className="testimonials-item-text">
+              “ It is a long established fact that a reader will be tracked distracted
+              by the readable content of a page is when looking at its layout. The point of using Lorem of distribution it look like readable English “
+            </p>
+            <div className="testimonials-item-user d-f jc-sb">
+              <div className='d-f'>
+                <div><img src="/images/testimonial-author2.png" alt="testimonial-author2" /></div>
+                <div style={{marginLeft: "20px"}}>
+                  <div className="testimonials-item-rating d-f">
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star.png" alt="star" /></div>
+                    <div><img src="/images/star-half.png" alt="star-half" /></div>
+                  </div>
+                  <div className="user-name">Lieo Jessica</div>
+                  <div className="user-country">United Kingdom</div>
+                </div>
+              </div>
+              <div><img src="/images/quote.png" alt="quote" /></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
