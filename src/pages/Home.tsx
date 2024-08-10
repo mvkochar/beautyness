bf152cbc189c97d39aea32c60141e64ceb286ba6
sgkochar <sgkochar@gmail.com>
2024-08-10 14:00:48 +0300

    add Benefits and Appointment blocks to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -168,7 +168,7 @@ const Home = () => {
             <div className="testimonials-item-user d-f jc-sb">
               <div className='d-f'>
                 <div><img src="/images/testimonial-author1.png" alt="testimonial-author1" /></div>
-                <div style={{marginLeft: "20px"}}>
+                <div style={{ marginLeft: "20px" }}>
                   <div className="testimonials-item-rating d-f">
                     <div><img src="/images/star.png" alt="star" /></div>
                     <div><img src="/images/star.png" alt="star" /></div>
@@ -191,7 +191,7 @@ const Home = () => {
             <div className="testimonials-item-user d-f jc-sb">
               <div className='d-f'>
                 <div><img src="/images/testimonial-author2.png" alt="testimonial-author2" /></div>
-                <div style={{marginLeft: "20px"}}>
+                <div style={{ marginLeft: "20px" }}>
                   <div className="testimonials-item-rating d-f">
                     <div><img src="/images/star.png" alt="star" /></div>
                     <div><img src="/images/star.png" alt="star" /></div>
@@ -207,6 +207,100 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </section>
+      <section className='home-benefits'>
+        <div className="above home-benefits-above">Our Awesome Benefits</div>
+        <h2 className="bl-title home-benefits-title">Actually what you'll get from</h2>
+        <div className="home-benefits-box d-f jc-sb">
+          <div className="home-benefits-item">
+            <div><img src="/images/benefits-icon.png" alt="benefits-icon" /></div>
+            <h4 className="benefits-item-title">Optimised Energy <br /> Efficiency</h4>
+            <p className="benefits-item-desc">
+              The point of using Lorem Ipsum is that  using making it look like readable.
+            </p>
+          </div>
+          <div className="home-benefits-item">
+            <div><img src="/images/benefits-icon.png" alt="benefits-icon" /></div>
+            <h4 className="benefits-item-title">Multi-faceted <br /> Water Filtration</h4>
+            <p className="benefits-item-desc">
+              The point of using Lorem Ipsum is that  using making it look like readable.
+            </p>
+          </div>
+          <div className="home-benefits-item">
+            <div><img src="/images/benefits-icon.png" alt="benefits-icon" /></div>
+            <h4 className="benefits-item-title">Uncompromising Quality <br /> and Durability</h4>
+            <p className="benefits-item-desc">
+              The point of using Lorem Ipsum is that  using making it look like readable.
+            </p>
+          </div>
+          <div className="home-benefits-item">
+            <div><img src="/images/benefits-icon.png" alt="benefits-icon" /></div>
+            <h4 className="benefits-item-title">An Ultra-Pampering Spa <br /> Experience</h4>
+            <p className="benefits-item-desc">
+              The point of using Lorem Ipsum is that  using making it look like readable.
+            </p>
+          </div>
+          <div className="home-benefits-item">
+            <div><img src="/images/benefits-icon.png" alt="benefits-icon" /></div>
+            <h4 className="benefits-item-title">Easy Care for Unparalleled <br /> Enjoyment</h4>
+            <p className="benefits-item-desc">
+              The point of using Lorem Ipsum is that  using making it look like readable.
+            </p>
+          </div>
+          <div className="home-benefits-item">
+            <div><img src="/images/benefits-icon.png" alt="benefits-icon" /></div>
+            <h4 className="benefits-item-title">Optimised Energy <br /> Efficiency</h4>
+            <p className="benefits-item-desc">
+              The point of using Lorem Ipsum is that  using making it look like readable.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="home-appointment d-f jc-sb align-center">
+        <div>
+          <div className="above">Flat Discount</div>
+          <h2 className="bl-title home-appointment-title">Claim upto 50% off er on‍ the most popular services...</h2>
+          <p className="home-appointment-desc">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+            alteration in some form, buying to injected humour, or randomised words.
+          </p>
+          <div className='home-appointment-content d-f'>
+            <div className="home-appointment-contact d-f">
+              <div className="appointment-contact-item d-f">
+                <div><img src="/images/pattern.svg" alt="pattern" /></div>
+                <dl>
+                  <dt>Mail us :</dt>
+                  <dd>info@beautyness.com</dd>
+                </dl>
+              </div>
+              <div className="appointment-contact-item d-f">
+                <div><img src="/images/pattern.svg" alt="pattern" /></div>
+                <dl>
+                  <dt>Call us :</dt>
+                  <dd>(+22) 123 4568 009</dd>
+                </dl>
+              </div>
+            </div>
+            <div className="home-appointment-offer">
+              <div className="appointment-offer-title">Get flat</div>
+              <div><img src="/images/offer.png" alt="offer" /></div>
+              <div className="appointment-offer-subtitle">Discount</div>
+            </div>
+          </div>
+        </div>
+        <form action="" className='home-appointment-fm'>
+          <input type="text" name='appointmentName' placeholder='Name' />
+          <input type="email" name='appointmentEmail' placeholder='Email' />
+          <input type="tel" name="appointmentPhone" placeholder='Phone' />
+          <input type="text" name='appointmentService' placeholder='Service You Need' />
+          <textarea name="appointmentNote" placeholder='Any Note For Us'></textarea>
+          <a href="">Get an appointment</a>
+        </form>
+      </section>
+      <section className="home-blog">
+        <div className="above text-center">Our Blog</div>
+        <h2 className="bl-title text-center">Latest Updates</h2>
+        <div className="home-blog-box d-f jc-sb"></div>
       </section>
     </>
   )
